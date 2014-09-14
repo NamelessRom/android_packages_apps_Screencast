8db2fb7ea871e69dcec8ad1e8086654d8110e1c1
Evisceration <eviscerationls@gmail.com>
2014-09-14 23:07:06 +0200

    only use one button at the dialog
    Change-Id: I1e563d3f5e81b499e77bad1c8cf957d937263bab

--- app/src/main/java/org/namelessrom/screencast/MainActivity.java
+++ app/src/main/java/org/namelessrom/screencast/MainActivity.java
@@ -24,43 +24,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.namelessrom.screencast.services.ScreencastService;
 
 public class MainActivity extends Activity {
 
-    private Button mStartScreencastButton;
-    private Button mStopScreencastButton;
+    private Button   mScreencastButton;
+    private TextView mStatusText;
 
     @Override protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mStartScreencastButton = ((Button) findViewById(R.id.start_screencast));
-        mStartScreencastButton.setOnClickListener(new View.OnClickListener() {
+        mScreencastButton = ((Button) findViewById(R.id.button_screencast));
+        mScreencastButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                Utils.setRecording(MainActivity.this, true);
+                final boolean isRecording = Utils.isRecording(MainActivity.this);
 
-                final Intent i = new Intent(ScreencastService.ACTION_START_SCREENCAST);
+                Utils.setRecording(MainActivity.this, !isRecording);
+
+                final Intent i = new Intent();
+                i.setAction(isRecording
+                        ? ScreencastService.ACTION_STOP_SCREENCAST
+                        : ScreencastService.ACTION_START_SCREENCAST);
                 i.setClass(MainActivity.this, ScreencastService.class);
                 startService(i);
 
-                finish();
+                if (isRecording) {
+                    refreshState();
+                } else {
+                    finish();
+                }
             }
         });
 
-        mStopScreencastButton = ((Button) findViewById(R.id.stop_screencast));
-        mStopScreencastButton.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                Utils.setRecording(MainActivity.this, false);
-
-                final Intent i = new Intent(ScreencastService.ACTION_STOP_SCREENCAST);
-                i.setClass(MainActivity.this, ScreencastService.class);
-                startService(i);
-
-                refreshState();
-            }
-        });
+        mStatusText = ((TextView) findViewById(R.id.statustext_screencast));
     }
 
     @Override public void onWindowFocusChanged(final boolean hasFocus) {
@@ -75,11 +74,13 @@ public class MainActivity extends Activity {
 
     private void refreshState() {
         final boolean isRecording = Utils.isRecording(this);
-        if (mStartScreencastButton != null) {
-            mStartScreencastButton.setEnabled(!isRecording);
+        if (mScreencastButton != null) {
+            mScreencastButton.setText(isRecording ? R.string.stop : R.string.start_screencast);
         }
-        if (mStopScreencastButton != null) {
-            mStopScreencastButton.setEnabled(isRecording);
+        if (mStatusText != null) {
+            mStatusText.setText(isRecording
+                    ? getString(R.string.stop_description, getString(R.string.stop))
+                    : getString(R.string.start_description, getString(R.string.start_screencast)));
         }
     }
 
